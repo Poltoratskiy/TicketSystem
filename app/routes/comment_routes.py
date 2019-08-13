07165412f42c0905f0a500c57e1ae5913819b0d9
Michael <m.poltoratskiy@yandex.ru>
2019-08-13 13:33:42 +0300

    refactored & cleaned

--- app/routes/comment_routes.py
+++ app/routes/comment_routes.py
@@ -1,8 +1,11 @@
 from flask import current_app as app
+import json
+from flask import request, abort
+from sqlalchemy import exc
+import traceback
+
 from app.model.ticket import Ticket, db
 from app.model.comment import Comment
-import json
-from flask import request
 
 
 @app.route('/comments', methods=['POST'])
@@ -13,33 +16,26 @@ def create_comment():
     """
     data = json.loads(request.data)
     ticket_id = data.get('ticket_id')
-    text = str(data.get('text'))
-    email = str(data.get('email'))
+    text = data.get('text')
+    email = data.get('email')
     if not data or ticket_id is None or email is None or text is None:
-        response = app.response_class(status=400,
-                                      mimetype='application/json')
-        return response
+        abort(400)
     try:
         ticket = Ticket.query.filter_by(id=ticket_id).first()
         if ticket is None:
-            response = app.response_class(status=400,
-                                          mimetype='application/json')
-            return response
-        if ticket.status.name == 'закрыт':
-            response = app.response_class(status=403,
-                                          mimetype='application/json')
-            return response
+            abort(400)
+        if ticket.status.name == 'closed':
+            abort(403)
         comment = Comment(text=text, email=email, ticket_id=ticket_id)
         db.session.add(comment)
         db.session.commit()
-        response = app.response_class(response=comment.json(),
-                                      status=200,
-                                      mimetype='application/json')
-        return response
-    except Exception as e:
+        return app.response_class(response=comment.json(),
+                                  status=200,
+                                  mimetype='application/json')
+        
+    except exc.SQLAlchemyError:
+        print(f'''Error: \n {traceback.format_exc()}''')
         db.session.rollback()
-        response = app.response_class(status=501,
-                                      mimetype='application/json')
-        return response
+        abort(501)
     finally:
         db.session.close()
